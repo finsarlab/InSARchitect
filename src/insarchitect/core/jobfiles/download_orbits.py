2283602abd3d8bbe8c774364e017f6bc3b970e8c
Daruma <hectoryoramquiroz@gmail.com>
2026-01-15 12:11:06 -0600

    feat: download orbits init

--- src/insarchitect/core/jobfiles/download_orbits.py
+++ src/insarchitect/core/jobfiles/download_orbits.py
@@ -2,4 +2,6 @@ from ...models import ProjectConfig
 import boto3
 
 def download_orbits(config: ProjectConfig):
-    print(config)
+    s3 = boto3.resource('s3')
+    for bucket in s3.buckets.all():
+        print(bucket.name)
