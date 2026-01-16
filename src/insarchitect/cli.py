86f5e436524da322352cbc6bd761e4128373906e
LEGM2002 <luiseduardogarciamartinez92@gmail.com>
2026-01-16 13:21:03 -0600

    feat: dem downloading implementation

--- src/insarchitect/cli.py
+++ src/insarchitect/cli.py
@@ -1,9 +1,10 @@
 import typer
-from .commands import download, jobfiles
+from .commands import download, jobfiles, dem
 
 app = typer.Typer()
 
 app.add_typer(download.app)
+app.add_typer(dem.app)
 app.add_typer(jobfiles.app)
 
 if __name__ == "__main__":
